f749724c2b3455da90ee88fec03e6fda06907c32
ahdadzia <ahda.fakhri@gmail.com>
2021-03-01 10:14:09 +0700

    Initial Commit

---
+++ Food App/Food_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_AppApp.swift
+//  Food App
+//
+//  Created by Ahda  Dzia Ulhaq on 01/03/21.
+//
+
+import SwiftUI
+
+@main
+struct Food_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
